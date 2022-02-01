8d754a41497a9d97985d949da6752aaf83e7f757
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-02-01 16:36:51 +0000

    chore(jest): improve config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,34 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/*.config.js',
+    '!**/*.config.ts',
+    '!**/config.ts',
+  ],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '^[@|~]/(.*)$': '<rootDir>/$1',
+  },
+//  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/.nuxt',
+    '<rootDir>/cypress',
+    '<rootDir>/coverage',
+    '<rootDir>/plugins',
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest'],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    'node_modules/(?!@mylibrary/)',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
 }
